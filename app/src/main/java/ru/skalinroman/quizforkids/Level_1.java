4951eb15532326ff97c11c279994691245a70b57
Skalin Roman <skalinroman@mail.ru>
2020-12-26 14:14:11 +0300

    Level_1 part_3

--- app/src/main/java/ru/skalinroman/quizforkids/Level_1.java
+++ app/src/main/java/ru/skalinroman/quizforkids/Level_1.java
@@ -2,9 +2,11 @@ package ru.skalinroman.quizforkids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level_1 extends AppCompatActivity {
 
@@ -13,10 +15,26 @@ public class Level_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
 
-        // Развернуть игру на весь экран - начало
+        final ImageView img_left = (ImageView) findViewById(R.id.amg_left);
+        img_left.setClipToOutline(true);   // Код, который скругляет углы левой картинки
+
+        final ImageView img_right = (ImageView) findViewById(R.id.amg_right);
+        img_right.setClipToOutline(true);   // Код, который скругляет углы правой картинки
+
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        // Развернуть игру на весь экран - конец
     }
+
+    // Системная кнопка "Назад" - начало
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(Level_1.this, GameLevels.class);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+        }
+    }
+    // Системная кнопка "Назад" - конец
 }
